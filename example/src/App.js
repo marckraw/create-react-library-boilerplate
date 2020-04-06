fd17cf890834733360e3dbbd18ca7eb3b7e89d7a
Marcin Krawczyk <marcin.krawczyk@ef.com>
2020-04-06 13:22:45 +0200

    init create-react-library@3.0.6

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'create-react-library-boilerplate'
+import 'create-react-library-boilerplate/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
