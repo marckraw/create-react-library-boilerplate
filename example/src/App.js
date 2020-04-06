a4a346e74fc9f78e576c104520a51ae3bf4bfc52
Marcin Krawczyk <marcin.krawczyk@ef.com>
2020-04-06 13:36:00 +0200

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from "react";
 
-import { ExampleComponent } from 'create-react-library-boilerplate'
-import 'create-react-library-boilerplate/dist/index.css'
+import SomethingWithFade from "./SomethingWithFade";
+import "create-react-library-boilerplate/dist/index.css";
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  return (
+    <div>
+      <div style={{ height: "300px", backgroundColor: "red" }}>
+        <h2>something 1</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "blue" }}>
+        <h2>something 2</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "red" }}>
+        <h2>something 1</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "blue" }}>
+        <h2>something 2</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "red" }}>
+        <h2>something 1</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "blue" }}>
+        <h2>something 2</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "red" }}>
+        <h2>something 1</h2>
+      </div>
+      <div style={{ height: "300px", backgroundColor: "blue" }}>
+        <h2>something 2</h2>
+      </div>
+      <SomethingWithFade />
+    </div>
+  );
+};
 
-export default App
+export default App;
